501b91ec5e86b44b5677e2e608e6e146b8c5d1e6
gaotang <gaotang@192.168.1.107>
2014-11-01 20:02:08 +0800

    add

---
+++ io/src/main/java/com/ijoyful/jdk/App.java
@@ -0,0 +1,13 @@
+package com.ijoyful.jdk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ thread/src/main/java/com/ijoyful/jdk/Actor.java
@@ -0,0 +1,8 @@
+package com.ijoyful.jdk;
+
+public class Actor {
+
+	public static void main(String[] args){
+		System.out.println("中国");
+	}
+}
